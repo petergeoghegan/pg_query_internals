84de1d7d1cb6869a7dc3c4347a2ff610350db6e5
Peter Geoghegan <pg@bowt.ie>
2019-03-30 19:10:33 -0700

    Add script to generate demo graphs

---
+++ generate-demo-graphs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+rm *.svg
+NAME="lehman-yao-orthodox-btree"
+echo "Generating $NAME.svg..."
+dot -T svg "$NAME.dot" -o "$NAME.svg"
+NAME="lehman-yao-orthodox-btree-suffix"
+echo "Generating $NAME.svg..."
+dot -T svg "$NAME.dot" -o "$NAME.svg"
+NAME="lehman-yao-orthodox-btree-suffix-short"
+echo "Generating $NAME.svg..."
+dot -T svg "$NAME.dot" -o "$NAME.svg"
